644cdf671341ef3cd7f492491bfee7e5da4ff193
Eliezer <eliezer@trajanux.com.br>
2016-06-08 09:21:53 -0300

    alteração do banco de dados

--- trajanux.sql
+++ trajanux.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: mysql08-farm59.uni5.net
--- Tempo de geração: 23/05/2016 às 14:35
+-- Tempo de geração: 08/06/2016 às 09:20
 -- Versão do servidor: 5.5.40-log
 -- Versão do PHP: 5.3.28
 
@@ -19,56 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `trajanux`
 --
-
-DELIMITER $$
---
--- Procedimentos
---
-CREATE DEFINER=`trajanux`@`%` PROCEDURE `atualizarTempo`(ident INTEGER, tempo FLOAT)
-    DETERMINISTIC
-BEGIN
-
-
-     Update organizer_Tarefas t set t.realizado = t.realizado +  tempo where t.id = ident;
-     select id_otarefas into @identificador from  organizer_Tarefas ot where ot.id = ident;
-
-  
-
-IF (@identificador <> 0) THEN
-   
-    call atualizarTempo(@identificador, tempo);
-
-END IF;
-
-   
-
-    END$$
-
---
--- Funções
---
-CREATE DEFINER=`trajanux`@`%` FUNCTION `QuantidadeTotal`(
-        `mes` VARCHAR(10),
-        `tipo` INTEGER
-    ) RETURNS float
-    DETERMINISTIC
-    SQL SECURITY INVOKER
-BEGIN
-declare ValorTotal float;
-select 
- sum(tarefas.valor) AS total
-into ValorTotal from
- tarefas
-where
- upper(tarefas.mes) = upper(mes)
- and tarefas.tipo = tipo
- and tarefas.ano = @ano_Referencia
-group by tarefas.mes , tarefas.tipo;
-
-RETURN ROUND(ValorTotal,2);
-END$$
-
-DELIMITER ;
+CREATE DATABASE IF NOT EXISTS `trajanux` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `trajanux`;
 
 -- --------------------------------------------------------
 
@@ -76,6 +28,7 @@ DELIMITER ;
 -- Estrutura para tabela `tarefas`
 --
 
+DROP TABLE IF EXISTS `tarefas`;
 CREATE TABLE IF NOT EXISTS `tarefas` (
   `data` varchar(100) NOT NULL,
   `mes` varchar(100) NOT NULL,
@@ -88,7 +41,8 @@ CREATE TABLE IF NOT EXISTS `tarefas` (
   `banco` text NOT NULL,
   `agenciacontacartao` varchar(500) NOT NULL,
   `tiporegistro` varchar(500) NOT NULL,
-  `identificacao` varchar(500) NOT NULL
+  `identificacao` varchar(500) NOT NULL,
+  `usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AVG_ROW_LENGTH=266;
 
 -- --------------------------------------------------------
@@ -97,12 +51,14 @@ CREATE TABLE IF NOT EXISTS `tarefas` (
 -- Estrutura para tabela `tipo`
 --
 
+DROP TABLE IF EXISTS `tipo`;
 CREATE TABLE IF NOT EXISTS `tipo` (
   `id` int(11) NOT NULL,
   `descricao` varchar(200) NOT NULL,
   `tipo` int(11) NOT NULL,
-  `situacao` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=latin1 AVG_ROW_LENGTH=481;
+  `situacao` int(11) NOT NULL DEFAULT '1',
+  `usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1 AVG_ROW_LENGTH=481;
 
 -- --------------------------------------------------------
 
@@ -110,10 +66,12 @@ CREATE TABLE IF NOT EXISTS `tipo` (
 -- Estrutura para tabela `tipo_id`
 --
 
+DROP TABLE IF EXISTS `tipo_id`;
 CREATE TABLE IF NOT EXISTS `tipo_id` (
   `doc` int(11) NOT NULL,
   `id_tarefa` int(11) DEFAULT NULL,
-  `id_tipo` int(11) DEFAULT NULL
+  `id_tipo` int(11) DEFAULT NULL,
+  `usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -122,13 +80,15 @@ CREATE TABLE IF NOT EXISTS `tipo_id` (
 -- Estrutura para tabela `tipo_palavras`
 --
 
+DROP TABLE IF EXISTS `tipo_palavras`;
 CREATE TABLE IF NOT EXISTS `tipo_palavras` (
   `id` int(11) NOT NULL,
   `descricao` varchar(200) NOT NULL,
   `tipo` int(11) NOT NULL,
   `PHPSESSID` varchar(200) NOT NULL,
-  `situacao` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=630 DEFAULT CHARSET=latin1 AVG_ROW_LENGTH=103;
+  `situacao` int(11) NOT NULL,
+  `usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=704 DEFAULT CHARSET=latin1 AVG_ROW_LENGTH=103;
 
 -- --------------------------------------------------------
 
@@ -136,10 +96,30 @@ CREATE TABLE IF NOT EXISTS `tipo_palavras` (
 -- Estrutura para tabela `tipo_tarefas`
 --
 
+DROP TABLE IF EXISTS `tipo_tarefas`;
 CREATE TABLE IF NOT EXISTS `tipo_tarefas` (
   `tipo` int(11) NOT NULL,
-  `id` varchar(100) NOT NULL
+  `id` varchar(100) NOT NULL,
+  `usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AVG_ROW_LENGTH=372;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `sexo` char(1) NOT NULL,
+  `data_nasc` date NOT NULL,
+  `grupo` varchar(128) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Índices de tabelas apagadas
@@ -176,6 +156,12 @@ ALTER TABLE `tipo_tarefas`
   ADD UNIQUE KEY `item` (`id`);
 
 --
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas apagadas
 --
 
@@ -183,12 +169,17 @@ ALTER TABLE `tipo_tarefas`
 -- AUTO_INCREMENT de tabela `tipo`
 --
 ALTER TABLE `tipo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=80;
 --
 -- AUTO_INCREMENT de tabela `tipo_palavras`
 --
 ALTER TABLE `tipo_palavras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=630;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=704;
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
